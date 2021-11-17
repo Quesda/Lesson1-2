72ea9e5c36e8ef27dde5c2ee660fe8c3c8e9317b
Артём <47342908+Quesda@users.noreply.github.com>
2021-11-17 20:59:25 +1000

    Update Main.java

--- Game/Main.java
+++ Game/Main.java
@@ -3,15 +3,17 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Добро пожаловать. Ваша задача угадать число!");
         System.out.println("Введите разброс чисел от нуля до: ");
         int range = scanner.nextInt();
         int number = (int) (Math.random() * range);
         System.out.println("Угадайте число от 0 до " + range);
-
+        playLevel(range, number);
+        scanner.close();
+    }
+    private static void playLevel (int range, int number) {
         while (true) {
             int input_number = scanner.nextInt();
             if (input_number == number) {
@@ -21,6 +23,5 @@ public class Main {
                 System.out.println("Загаданное число меньше " + input_number + " Попробуйте ещё раз");
             } else System.out.println("Загаданное число больше " + input_number + " Попробуйте ещё раз");
         }
-        scanner.close();
     }
 }
